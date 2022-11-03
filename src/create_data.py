335ec5848ce528997fd168fdfcd41982c8704ae7
Nick Sorros <nsorros@gmail.com>
2022-11-03 16:24:28 +0200

    Fix create data for folds and per data

--- src/create_data.py
+++ src/create_data.py
@@ -1,10 +1,11 @@
+from pathlib import Path
 import os
 
 import srsly
 import typer
 
 
-from src.data import load_data, sample_fold
+from src.data import load_data, sample_data
 
 app = typer.Typer()
 
@@ -14,21 +15,22 @@ def create_data(
     data_dir="data",
     dataset="imdb",
     n_shot: int = 8,
+    n_folds: int = 5,
     test_size: int = 100,
     split="train",
 ):
-    data = load_data(dataset, test_size)
+    data = load_data(dataset)
 
-    num_classes = len(set(data["train"]["label"]))
-    sample_size = num_classes * n_shot
+    data_dir = os.path.join(data_dir, dataset, str(n_shot))
+    Path(data_dir).mkdir(parents=True, exist_ok=True)
 
-    if split == "train":
-        data = sample_fold(data["train"], 1, sample_size)
-    else:
-        data = data["test"]
+    for fold in range(n_folds):
+        sample_dataset = sample_data(
+            data, num_samples_per_class=n_shot, test_size=test_size, seed=fold
+        )
 
-    data_path = os.path.join(data_dir, f"{dataset}_{n_shot}shot_{split}.jsonl")
-    srsly.write_jsonl(data_path, data)
+        data_path = os.path.join(data_dir, f"fold{fold}_{split}.jsonl")
+        srsly.write_jsonl(data_path, sample_dataset[split])
 
 
 if __name__ == "__main__":
