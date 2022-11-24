9f655e45e161805236eff1d218e198670b34ea4b
Nick Sorros <nsorros@gmail.com>
2022-11-24 14:16:25 +0200

    Update record_manual and record sota

--- src/record_manual.py
+++ src/record_manual.py
@@ -5,12 +5,12 @@ import os
 import typer
 
 
-def record_autotrain(dataset, nshot: int, results, results_dir="results"):
+def record_manual(dataset, approach, nshot, results, results_dir="results"):
     result_data = []
     for fold, result in enumerate(results.split(",")):
         result_data.append({"fold": fold, "metrics": {"accuracy": float(result)}})
 
-    results_path = os.path.join(results_dir, dataset, "autotrain")
+    results_path = os.path.join(results_dir, dataset, approach)
     Path(results_path).mkdir(exist_ok=True, parents=True)
 
     with open(os.path.join(results_path, f"{nshot}.json"), "w") as f:
@@ -18,4 +18,4 @@ def record_autotrain(dataset, nshot: int, results, results_dir="results"):
 
 
 if __name__ == "__main__":
-    typer.run(record_autotrain)
+    typer.run(record_manual)
